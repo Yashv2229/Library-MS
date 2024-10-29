142cfa8f1c49c0ce6787929574c05de253ea1a62
Pavan Das <Pavandas1029@gmail.com>
2024-10-29 10:30:08 +0530

    formatted

--- request-a-book.php
+++ request-a-book.php
@@ -2,15 +2,14 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if(strlen($_SESSION['login'])==0)
-    {   
-header('location:index.php');
+if (strlen($_SESSION['login']) == 0) {
+    header('location:index.php');
+} else {
 }
-else{ 
-}
-    ?>
+?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
@@ -29,59 +28,57 @@ else{
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 
 </head>
+
 <body>
-      <!------MENU SECTION START-->
-<?php include('includes/header.php');?>
-<!-- MENU SECTION END-->
+    <!------MENU SECTION START-->
+    <?php include('includes/header.php'); ?>
+    <!-- MENU SECTION END-->
     <div class="content-wrapper">
-         <div class="container">
-        <div class="row pad-botm">
-            <div class="col-md-12">
-                <h4 class="header-line">Request a Book</h4>
-    </div>
-     <div class="row">
-    <?php if($_SESSION['error']!="")
-    {?>
-<div class="col-md-6">
-<div class="alert alert-danger" >
- <strong>Error :</strong> 
- <?php echo htmlentities($_SESSION['error']);?>
-<?php echo htmlentities($_SESSION['error']="");?>
-</div>
-</div>
-<?php } ?>
-<?php if($_SESSION['msg']!="")
-{?>
-<div class="col-md-6">
-<div class="alert alert-success" >
- <strong>Success :</strong> 
- <?php echo htmlentities($_SESSION['msg']);?>
-<?php echo htmlentities($_SESSION['msg']="");?>
-</div>
-</div>
-<?php } ?>
+        <div class="container">
+            <div class="row pad-botm">
+                <div class="col-md-12">
+                    <h4 class="header-line">Request a Book</h4>
+                </div>
+                <div class="row">
+                    <?php if ($_SESSION['error'] != "") { ?>
+                        <div class="col-md-6">
+                            <div class="alert alert-danger">
+                                <strong>Error :</strong>
+                                <?php echo htmlentities($_SESSION['error']); ?>
+                                <?php echo htmlentities($_SESSION['error'] = ""); ?>
+                            </div>
+                        </div>
+                    <?php } ?>
+                    <?php if ($_SESSION['msg'] != "") { ?>
+                        <div class="col-md-6">
+                            <div class="alert alert-success">
+                                <strong>Success :</strong>
+                                <?php echo htmlentities($_SESSION['msg']); ?>
+                                <?php echo htmlentities($_SESSION['msg'] = ""); ?>
+                            </div>
+                        </div>
+                    <?php } ?>
 
 
 
-   <?php if($_SESSION['delmsg']!="")
-    {?>
-<div class="col-md-6">
-<div class="alert alert-success" >
- <strong>Success :</strong> 
- <?php echo htmlentities($_SESSION['delmsg']);?>
-<?php echo htmlentities($_SESSION['delmsg']="");?>
-</div>
-</div>
-<?php } ?>
+                    <?php if ($_SESSION['delmsg'] != "") { ?>
+                        <div class="col-md-6">
+                            <div class="alert alert-success">
+                                <strong>Success :</strong>
+                                <?php echo htmlentities($_SESSION['delmsg']); ?>
+                                <?php echo htmlentities($_SESSION['delmsg'] = ""); ?>
+                            </div>
+                        </div>
+                    <?php } ?>
 
-</div>
-        </div>
+                </div>
+            </div>
             <div class="row">
                 <div class="col-md-12">
                     <!-- Advanced Tables -->
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                          Available Books 
+                            Available Books
                         </div>
                         <div class="panel-body">
                             <div class="table-responsive">
@@ -90,37 +87,37 @@ else{
                                         <tr>
                                             <th>#</th>
                                             <th>Book Name</th>
-											<th>Category</th>
-											<th>Publication Name</th>
+                                            <th>Category</th>
+                                            <th>Publication Name</th>
                                             <th>ISBN </th>
                                             <th>Price</th>
                                             <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-<?php $sql = "SELECT tblbooks.BookName,tblbooks.Copies,tblbooks.IssuedCopies,tblcategory.CategoryName,tblauthors.AuthorName,tblbooks.ISBNNumber,tblbooks.BookPrice,tblbooks.id as bookid from  tblbooks join tblcategory on tblcategory.id=tblbooks.CatId join tblauthors on tblauthors.id=tblbooks.AuthorId";
-$query = $dbh -> prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{     
-if($result->Copies > $result->IssuedCopies)
-{          ?>                               
-                                        <tr class="odd gradeX">
-										    
-                                            <td class="center"><?php echo htmlentities($cnt);?></td>
-                                            <td class="center"><?php echo htmlentities($result->BookName);?></td>
-                                            <td class="center"><?php echo htmlentities($result->CategoryName);?></td>
-                                            <td class="center"><?php echo htmlentities($result->AuthorName);?></td>
-                                            <td class="center"><?php echo htmlentities($result->ISBNNumber);?></td>
-                                            <td class="center"><?php echo htmlentities($result->BookPrice);?></td>
-											<td class="center"><a href="temp.php?ISBNNumber=<?php echo $result->ISBNNumber;?>&BookName=<?php echo $result->BookName;?>&AuthorName=<?php echo $result->AuthorName;?>&CategoryName=<?php echo $result->CategoryName;?>&BookPrice=<?php echo $result->BookPrice;?>&StudName=<?php echo $_SESSION['username'];?>&StudentID=<?php echo $_SESSION['stdid'];?>
-											"><button class="btn btn-primary" name="submit" id="submit" type="submit"><i class="fa fa-edit "></i> Request</button></td>		
-                                        </tr>
-<?php $cnt=$cnt+1;}}} ?>                                      
+                                        <?php $sql = "SELECT tblbooks.BookName,tblbooks.Copies,tblbooks.IssuedCopies,tblcategory.CategoryName,tblauthors.AuthorName,tblbooks.ISBNNumber,tblbooks.BookPrice,tblbooks.id as bookid from  tblbooks join tblcategory on tblcategory.id=tblbooks.CatId join tblauthors on tblauthors.id=tblbooks.AuthorId";
+                                        $query = $dbh->prepare($sql);
+                                        $query->execute();
+                                        $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                        $cnt = 1;
+                                        if ($query->rowCount() > 0) {
+                                            foreach ($results as $result) {
+                                                if ($result->Copies > $result->IssuedCopies) {          ?>
+                                                    <tr class="odd gradeX">
+
+                                                        <td class="center"><?php echo htmlentities($cnt); ?></td>
+                                                        <td class="center"><?php echo htmlentities($result->BookName); ?></td>
+                                                        <td class="center"><?php echo htmlentities($result->CategoryName); ?></td>
+                                                        <td class="center"><?php echo htmlentities($result->AuthorName); ?></td>
+                                                        <td class="center"><?php echo htmlentities($result->ISBNNumber); ?></td>
+                                                        <td class="center"><?php echo htmlentities($result->BookPrice); ?></td>
+                                                        <td class="center"><a href="temp.php?ISBNNumber=<?php echo $result->ISBNNumber; ?>&BookName=<?php echo $result->BookName; ?>&AuthorName=<?php echo $result->AuthorName; ?>&CategoryName=<?php echo $result->CategoryName; ?>&BookPrice=<?php echo $result->BookPrice; ?>&StudName=<?php echo $_SESSION['username']; ?>&StudentID=<?php echo $_SESSION['stdid']; ?>
+											"><button class="btn btn-primary" name="submit" id="submit" type="submit"><i class="fa fa-edit "></i> Request</button></td>
+                                                    </tr>
+                                        <?php $cnt = $cnt + 1;
+                                                }
+                                            }
+                                        } ?>
                                     </tbody>
                                 </table>
                             </div>
@@ -128,11 +125,11 @@ if($result->Copies > $result->IssuedCopies)
                     </div>
                     <!--End Advanced Tables -->
                 </div>
-            </div>    
-    </div>
+            </div>
+        </div>
     </div>
 
-     <!-- CONTENT-WRAPPER SECTION END-->
+    <!-- CONTENT-WRAPPER SECTION END-->
     <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
     <!-- CORE JQUERY  -->
     <script src="assets/js/jquery-1.10.2.js"></script>
@@ -141,7 +138,8 @@ if($result->Copies > $result->IssuedCopies)
     <!-- DATATABLE SCRIPTS  -->
     <script src="assets/js/dataTables/jquery.dataTables.js"></script>
     <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
-      <!-- CUSTOM SCRIPTS  -->
+    <!-- CUSTOM SCRIPTS  -->
     <script src="assets/js/custom.js"></script>
 </body>
+
 </html>
